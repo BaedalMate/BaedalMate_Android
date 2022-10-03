4e144e73da87b980edd8da95383160c9a6a96263
DongJun-H <dongjun826@seoultech.ac.kr>
2022-10-03 21:13:33 +0900

    #24 [layout] Modify WriteViewModel
    issue: #24

--- app/src/main/java/com/mate/baedalmate/presentation/viewmodel/WriteViewModel.kt
+++ app/src/main/java/com/mate/baedalmate/presentation/viewmodel/WriteViewModel.kt
@@ -1,29 +1,12 @@
 package com.mate.baedalmate.presentation.viewmodel
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
-import com.mate.baedalmate.BuildConfig
-import com.mate.baedalmate.common.ListLiveData
-import com.mate.baedalmate.data.datasource.remote.recruit.DeliveryPlatform
-import com.mate.baedalmate.data.datasource.remote.recruit.Dormitory
-import com.mate.baedalmate.data.datasource.remote.recruit.MenuDto
-import com.mate.baedalmate.data.datasource.remote.recruit.PlaceDto
 import com.mate.baedalmate.data.datasource.remote.recruit.RecruitFinishCriteria
 import com.mate.baedalmate.data.datasource.remote.recruit.ShippingFeeDto
-import com.mate.baedalmate.data.datasource.remote.recruit.TagDto
-import com.mate.baedalmate.data.datasource.remote.write.PlaceMeta
-import com.mate.baedalmate.data.datasource.remote.write.RegionInfo
-import com.mate.baedalmate.data.datasource.remote.write.ResultSearchKeyword
-import com.mate.baedalmate.domain.usecase.write.RequestKakaoLocalUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
-import javax.inject.Inject
 
-@HiltViewModel
-class WriteViewModel @Inject constructor(private val kakaoLocalUseCase: RequestKakaoLocalUseCase) :
-    ViewModel() {
+class WriteViewModel : ViewModel() {
     var categoryId = 0
 
     var deadLinePeopleCount = 0
@@ -32,17 +15,4 @@ class WriteViewModel @Inject constructor(private val kakaoLocalUseCase: RequestK
     var deadLineCriterion = RecruitFinishCriteria.NUMBER
     var isDeliveryFeeFree = false
     var deliveryFeeRangeList = mutableListOf<ShippingFeeDto>()
-
-    var deliveryDormitory = Dormitory.NURI
-    var deliveryStore = MutableLiveData(PlaceDto("", "", "", 0f, 0f))
-    var deliveryPlatform = DeliveryPlatform.BAEMIN
-    var isCouponUse = false
-    var couponAmount = 0
-
-    var postTitle = ""
-    var postDetail = ""
-    var postTagList = mutableListOf<TagDto>()
-
-    var menuList = mutableListOf<MenuDto>()
-    var postMenuList = ListLiveData<List<String>>()
 }
