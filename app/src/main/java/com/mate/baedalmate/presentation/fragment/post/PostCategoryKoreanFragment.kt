63e425a295fa6c2e6bf1439a2bbd0a8954e6efb8
DongJun-H <dongjun826@seoultech.ac.kr>
2022-12-29 19:51:49 +0900

    #93 [layout] Create Category List Empty View
    resolved: #93

--- app/src/main/java/com/mate/baedalmate/presentation/fragment/post/PostCategoryKoreanFragment.kt
+++ app/src/main/java/com/mate/baedalmate/presentation/fragment/post/PostCategoryKoreanFragment.kt
@@ -1,10 +1,15 @@
 package com.mate.baedalmate.presentation.fragment.post
 
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -13,6 +18,7 @@ import com.bumptech.glide.RequestManager
 import com.mate.baedalmate.R
 import com.mate.baedalmate.common.autoCleared
 import com.mate.baedalmate.databinding.FragmentPostCategoryKoreanBinding
+import com.mate.baedalmate.databinding.ItemEmptyPostCategoryViewBinding
 import com.mate.baedalmate.presentation.fragment.post.adapter.PostCategoryListAdapter
 import com.mate.baedalmate.presentation.viewmodel.RecruitViewModel
 import dagger.hilt.android.AndroidEntryPoint
@@ -23,6 +29,7 @@ class PostCategoryKoreanFragment : Fragment() {
     private val recruitViewModel by activityViewModels<RecruitViewModel>()
     private lateinit var postCategoryListAdapter: PostCategoryListAdapter
     private lateinit var glideRequestManager: RequestManager
+    private val constraintSet = ConstraintSet()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +49,7 @@ class PostCategoryKoreanFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         getRecruitList()
         initListAdapter()
+        setCategoryListContents()
         setCategoryClickListener()
     }
 
@@ -60,8 +68,55 @@ class PostCategoryKoreanFragment : Fragment() {
         with(binding) {
             rvPostCategoryKoreanList.adapter = postCategoryListAdapter
         }
+    }
+
+    private fun setCategoryClickListener() {
+        binding.radiogroupLayoutPostCategoryKoreanSort.setOnCheckedChangeListener { group, checkedId ->
+            when (checkedId) {
+                R.id.radiobutton_post_category_korean_sort_time -> {
+                    getRecruitList(sort = "deadlineDate")
+                }
+                R.id.radiobutton_post_category_korean_sort_star -> {
+                    getRecruitList(sort = "score")
+                }
+                R.id.radiobutton_post_category_korean_sort_popular -> {
+                    getRecruitList(sort = "view")
+                }
+            }
+        }
+    }
+
+    private fun setCategoryListContents() {
+        val emptyPostCategoryViewBinding =
+            ItemEmptyPostCategoryViewBinding.inflate(LayoutInflater.from(binding.root.context))
+        emptyPostCategoryViewBinding.tvEmptyPostCategoryGuideNotAppear.text =
+            String.format(
+                getString(R.string.post_category_list_empty),
+                getString(R.string.category_korean)
+            )
+        val span =
+            SpannableString(emptyPostCategoryViewBinding.tvEmptyPostCategoryGuideNotAppear.text)
+        setEmptyViewMessage(span, getString(R.string.category_korean))
+        emptyPostCategoryViewBinding.tvEmptyPostCategoryGuideNotAppear.text = span
+
+        val emptyView = emptyPostCategoryViewBinding.root
+        addEmptyView(emptyView)
+
         recruitViewModel.recruitListKorean.observe(viewLifecycleOwner) { recruitList ->
-            postCategoryListAdapter.submitList(recruitList.recruitList.toMutableList())
+            if (recruitList.recruitList.isNotEmpty()) {
+                postCategoryListAdapter.submitList(recruitList.recruitList.toMutableList())
+                with(constraintSet) {
+                    clone(binding.layoutPostCategoryListKorean)
+                    setVisibility(emptyView.id, View.GONE)
+                    applyTo(binding.layoutPostCategoryListKorean)
+                }
+            } else {
+                with(constraintSet) {
+                    clone(binding.layoutPostCategoryListKorean)
+                    setVisibility(emptyView.id, View.VISIBLE)
+                    applyTo(binding.layoutPostCategoryListKorean)
+                }
+            }
         }
 
         postCategoryListAdapter.setOnItemClickListener(object :
@@ -76,13 +131,50 @@ class PostCategoryKoreanFragment : Fragment() {
         })
     }
 
-    private fun setCategoryClickListener() {
-        binding.radiogroupLayoutPostCategoryKoreanSort.setOnCheckedChangeListener { group, checkedId ->
-            when (checkedId) {
-                R.id.radiobutton_post_category_korean_sort_time -> { getRecruitList(sort = "deadlineDate") }
-                R.id.radiobutton_post_category_korean_sort_star -> { getRecruitList(sort = "score") }
-                R.id.radiobutton_post_category_korean_sort_popular -> { getRecruitList(sort = "view") }
-            }
-        }
+    private fun setEmptyViewMessage(span: SpannableString, categoryName: String) {
+        span.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(requireContext(), R.color.main_FB5F1C)),
+            span.indexOf(categoryName) - 1,
+            span.indexOf(categoryName) + categoryName.length + 1,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+    }
+
+    private fun addEmptyView(emptyView: View) {
+        binding.layoutPostCategoryListKorean.addView(emptyView)
+        setConstraintLayoutCondition(emptyView.id, binding.layoutPostCategoryListKorean.id)
+    }
+
+    private fun setConstraintLayoutCondition(childLayoutId: Int, parentLayoutId: Int) {
+        constraintSet.clone(binding.layoutPostCategoryListKorean)
+        constraintSet.connect(
+            childLayoutId,
+            ConstraintSet.TOP,
+            parentLayoutId,
+            ConstraintSet.TOP,
+            0
+        )
+        constraintSet.connect(
+            childLayoutId,
+            ConstraintSet.BOTTOM,
+            parentLayoutId,
+            ConstraintSet.BOTTOM,
+            0
+        )
+        constraintSet.connect(
+            childLayoutId,
+            ConstraintSet.START,
+            parentLayoutId,
+            ConstraintSet.START,
+            0
+        )
+        constraintSet.connect(
+            childLayoutId,
+            ConstraintSet.END,
+            parentLayoutId,
+            ConstraintSet.END,
+            0
+        )
+        constraintSet.applyTo(binding.layoutPostCategoryListKorean)
     }
 }
