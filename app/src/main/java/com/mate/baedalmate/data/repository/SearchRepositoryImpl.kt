2c138ca664908c646c0cd6f2c5614664a71ea7c4
DongJun-H <dongjun826@seoultech.ac.kr>
2023-01-23 00:13:14 +0900

    #196 [feature] Create Infinity Scroll with Recruit Post Result in SearchFragment
    resolved: #196

--- app/src/main/java/com/mate/baedalmate/data/repository/SearchRepositoryImpl.kt
+++ app/src/main/java/com/mate/baedalmate/data/repository/SearchRepositoryImpl.kt
@@ -1,23 +1,22 @@
 package com.mate.baedalmate.data.repository
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import com.mate.baedalmate.data.datasource.PostSearchResultPagingSource
 import com.mate.baedalmate.data.datasource.remote.search.SearchApiService
-import com.mate.baedalmate.domain.model.ApiResult
-import com.mate.baedalmate.domain.model.RecruitList
-import com.mate.baedalmate.domain.model.setExceptionHandling
+import com.mate.baedalmate.domain.model.RecruitDto
 import com.mate.baedalmate.domain.repository.SearchRepository
+import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class SearchRepositoryImpl @Inject constructor(private val searchApiService: SearchApiService) :
     SearchRepository {
     override suspend fun requestGetSearchTagKeyword(
         keyword: String,
-        page: Int,
-        size: Int,
         sort: String
-    ): ApiResult<RecruitList> =
-        setExceptionHandling {
-            searchApiService.requestGetSearchTagKeyword(
-                keyword = keyword, page = page, size = size, sort = sort
-            )
-        }
+    ): Flow<PagingData<RecruitDto>> = Pager(
+        config = PagingConfig(pageSize = 6, maxSize = 30, enablePlaceholders = false),
+        pagingSourceFactory = { PostSearchResultPagingSource(searchApiService, keyword, sort) }
+    ).flow
 }
